0745ac3c2b87493f7cabad42f24fcea4653b390f
Karlygash Zhuginissova <karlygash1510@gmail.com>
2017-07-05 13:08:26 -0700

    First commit: Project added

---
+++ GithubTutorial/GithubTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GithubTutorial
+//
+//  Created by Karlygash Zhuginissova on 7/5/17.
+//  Copyright © 2017 Karlygash Zhuginissova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
