54e13e184276cc9e7c2775563fce9e101dc6e49c
Karlygash Zhuginissova <karlygash1510@gmail.com>
2017-07-05 13:27:59 -0700

    change 1 from master

---
+++ GithubTutorial/GithubTutorial/MasterViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MasterViewController.swift
+//  GithubTutorial
+//
+//  Created by Karlygash Zhuginissova on 7/5/17.
+//  Copyright © 2017 Karlygash Zhuginissova. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
