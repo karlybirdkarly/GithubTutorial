f2c30dd7f67b9e8cb6af9d028e460308e83f4914
owasim1 <omar.wasim@valpo.edu>
2017-07-05 13:16:15 -0700

    change 1

--- GithubTutorial/GithubTutorial/ViewController.swift
+++ GithubTutorial/GithubTutorial/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Omar changed this
         // Do any additional setup after loading the view, typically from a nib.
     }
 
