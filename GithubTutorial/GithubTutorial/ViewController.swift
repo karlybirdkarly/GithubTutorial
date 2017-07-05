f9c9ea0a8a4a1b84d88637522d22d3c86aa2803d
owasim1 <omar.wasim@valpo.edu>
2017-07-05 13:22:42 -0700

    change 2

--- GithubTutorial/GithubTutorial/ViewController.swift
+++ GithubTutorial/GithubTutorial/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         //Omar changed this
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
